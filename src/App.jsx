735745d008f7e7915199befc3362e97663d34e07
MrRahmat <vovacet12@mail.ru>
2020-11-17 22:23:51 +0800

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import './scss/app.scss';
 import {Header} from './components';
 import {Home, Cart} from './pages';
-import {Route} from 'react-router-dom'
+import {Route} from 'react-router-dom';
 
 function App() {
+const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({data})=> {
+      setPizzas(data.pizzas);
+    });
+
+    /*
+    fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
+      setPizzas(json.pizzas);
+    });
+    */
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/cart' component={Cart}/>
+        <Route exact path='/' render={() => <Home items={pizzas} />}/>
+        <Route exact path='/cart' component={Cart} />
       </div>
     </div>
   );
